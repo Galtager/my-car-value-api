e519ebc56ce71c57c4712574d01ce17d19f4d995
gal tager <gal@alterno.io>
2024-08-31 18:15:34 +0300

    add sqlite entities

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { Report } from './report.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [ReportsService],
-  controllers: [ReportsController]
+  controllers: [ReportsController],
+  imports: [TypeOrmModule.forFeature([Report])]
+
 })
-export class ReportsModule {}
+export class ReportsModule { }
