2c368c9a598e7646ec8fbce51ba4254324a1db2b
gal tager <gal@alterno.io>
2024-09-03 20:33:24 +0300

    add estimate route with query builder

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -1,6 +1,7 @@
+import { Transform } from "class-transformer";
 import { IsLatitude, IsLongitude, IsNumber, IsString, Max, Min } from "class-validator";
 
-export class CreateReportDto {
+export class GetEstimateDto {
 
     @IsString()
     make: string;
@@ -8,26 +9,24 @@ export class CreateReportDto {
     @IsString()
     model: string;
 
+    @Transform(({ value }) => parseInt(value))
     @IsNumber()
     @Min(1930)
     @Max(2050)
     year: number;
 
+    @Transform(({ value }) => parseInt(value))
     @IsLongitude()
     lng: number;
 
+    @Transform(({ value }) => parseInt(value))
     @IsLatitude()
     lat: number;
 
+    @Transform(({ value }) => parseInt(value))
     @IsNumber()
     @Min(0)
     @Max(1000000)
     mileage: number;
-
-    @IsNumber()
-    @Min(0)
-    @Max(1000000)
-    price: number;
-
 
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { Request } from 'express';
+import { query, Request } from 'express';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -23,6 +24,11 @@ export class ReportsController {
     @UseGuards(AdminGuard)
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
         return this.reportService.changeApproval(id, body.approved)
+    }
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {
+        return this.reportService.createEstimate(query)
     }
 
 }
